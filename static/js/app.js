c83a64fbfa5e9fc2d5cd0f843efca4cf4e794189
megancoggs <m.cogguillo@gmail.com>
2020-12-01 23:30:16 -0500

    updated with working bubble chart script

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,7 @@
 buttonData = "../../data/samples.json"
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
 
-function buildPlot () {
+function buildPlots() {
 
     // Fetch the JSON data
     d3.json(buttonData).then(function(data) {
@@ -21,11 +16,7 @@ function buildPlot () {
         var sample_values = samples.sample_values;
         var otu_labels = samples.otu_labels;
         var otu_ids = samples.otu_ids;
-        
-        // // Log the values to the console
-        // console.log(`Sample Values: ${sample_values}`);
-        // console.log(`OTU Labels: ${otu_labels}`);
-        // console.log(`OTU IDs: ${otu_ids}`);
+        var otu_id_strings = otu_ids.map(id => `OTU ${id}`)
 
         // Grab top 10 OTUs found in individual
         // Note: sample values are sorted in descending order in teh dataset
@@ -33,13 +24,15 @@ function buildPlot () {
         var top_sample_values = sample_values.slice(0,10);
         var top_otu_labels = otu_labels.slice(0,10);
         var top_otu_ids = otu_ids.slice(0,10);
-        var top_otu_id_strings = top_otu_ids.map(id => `OTU ${id}`)
+        var top_otu_id_strings = otu_id_strings.slice(0,10);
   
         // Log the values to the console
         console.log(`Sample Values: ${top_sample_values}`);
         console.log(`OTU Labels: ${top_otu_labels}`);
         console.log(`OTU IDs: ${top_otu_ids}`);
         console.log(`OTU ID Strings: ${top_otu_id_strings}`);
+
+        // --------------------------- BAR CHART ---------------------------
 
         // Create trace for horiztonal bar chart with OTU ID strings as labels,
         // sample values as values, and OTU labels as hovertext
@@ -52,14 +45,41 @@ function buildPlot () {
         };
 
         // Plot the data
-        var plot_data = [trace1];
+        var plot_data1 = [trace1];
         var layout = {
             title: "Top 10 OTUs in Individual"
         };
-        Plotly.newPlot("bar", plot_data, layout);
+        Plotly.newPlot("bar", plot_data1, layout);
 
+        // --------------------------- BUBBLE CHART ---------------------------
+
+        var trace2 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+
+        // Plot the data
+        var plot_data2 = [trace2];
+        var layout = {
+            title: "Sample Value by OTU ID",
+            xaxis: {
+                title: {
+                    text: "OTU ID"
+                }
+            }
+
+        };
+        Plotly.newPlot("bubble", plot_data2, layout);
     });
 };
 
-buildPlot();
+
+buildPlots();
 
